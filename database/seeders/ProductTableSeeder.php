04f5fd8f81a9908721b11bdd64306e8f53a9d216
Mohamed Ismail <mohamedismail0994@gmail.com>
2023-11-25 13:53:52 +0200

    Update Product Seeder

--- database/seeders/ProductTableSeeder.php
+++ database/seeders/ProductTableSeeder.php
@@ -22,157 +22,110 @@ class ProductTableSeeder extends Seeder
         $now = now();
         Product::insert([
             [
-                'name'              => 'كوبن توباكو',
-                'url'               => 'https://laverne.com/ar/jYqGpy',
-                'image_url'         => 'https://cdn.salla.sa/XzOPD/jFqbTiFmPgQnUBhqGV7PCUk6ttugcMhGTB2PFW8V.png',
-                'category_url'      => 'https://laverne.com/category/yajOz',
+                'name'              => 'عطر وايلد كولت - 200 مل', // 7
+                'url'               => 'https://3saf.com/ar/Wll/p1370701860',
+                'image_url'         => 'https://cdn.salla.sa/yrlRO/iGve0Pnb6ZrG9u2vhJ0VghdbWWArqe3pcHa62CzL.jpg',
                 'created_at'        => $now,
                 'updated_at'        => $now,
             ],
             [
-                'name'              => 'بيانكو',
-                'url'               => 'https://laverne.com/ar/qWQYyO',
-                'image_url'         => 'https://cdn.salla.sa/XzOPD/7eFeWDM676ldtdJKRc8cL1E2PPFmH8u9Lyayv5UQ.png',
-                'category_url'      => 'https://laverne.com/category/yajOz',
+                'name'              => 'عطر نوماد 200 مل', // 7
+                'url'               => 'https://3saf.com/ar/nom/p558544826',
+                'image_url'         => 'https://cdn.salla.sa/yrlRO/28MfdnnG19OlSr26L2NsUkvB1Yt5GTYZJwlpYZoN.jpg',
                 'created_at'        => $now,
                 'updated_at'        => $now,
             ],
             [
-                'name'              => 'توباكو',
-                'url'               => 'https://laverne.com/ar/oOKrVB',
-                'image_url'         => 'https://cdn.salla.sa/XzOPD/D5a9jhLheYCtTcCw6j8R2JjW0nOcDmc6qCgEh3ZN.png',
-                'category_url'      => 'https://laverne.com/category/yajOz',
+                'name'              => 'عطر مس ساكورا - 200 مل', // 0
+                'url'               => 'https://3saf.com/ar/s0d/p71799739',
+                'image_url'         => 'https://cdn.salla.sa/yrlRO/cOnw6s2S4wGmzTcFWKtFkRxoaFgWmyr5lbuCh3RG.jpg',
                 'created_at'        => $now,
                 'updated_at'        => $now,
             ],
             [
-                'name'              => 'فانتوم',
-                'url'               => 'https://laverne.com/ar/xjojVg',
-                'image_url'         => 'https://cdn.salla.sa/XzOPD/AnEtsjhsMyGfRqOYWB1Qb1kwQ3P7d5RMOfULeO6w.png',
-                'category_url'      => 'https://laverne.com/category/yajOz',
+                'name'              => 'عطر يوني سبريت -200مل', // 3
+                'url'               => 'https://3saf.com/ar/yon/p427598918',
+                'image_url'         => 'https://cdn.salla.sa/yrlRO/TLs7uk9aUdVHLbPcUxH9U0FU8yFqKHmhbVticXru.jpg',
                 'created_at'        => $now,
                 'updated_at'        => $now,
             ],
             [
-                'name'              => 'آي واز هير',
-                'url'               => 'https://laverne.com/ar/BAylBR',
-                'image_url'         => 'https://cdn.salla.sa/XzOPD/lXwLU77zAr70xR2s9ondvTb7LZV7aGK018R2jIZa.png',
-                'category_url'      => 'https://laverne.com/category/yajOz',
+                'name'              => 'عطر بنك ليدي - 200مل', // 3
+                'url'               => 'https://3saf.com/ar/wli/p726698752',
+                'image_url'         => 'https://cdn.salla.sa/yrlRO/PbC4OlSEhSL9A7WIlbQnwSI5PwDrtyAWi1ag36qj.jpg',
                 'created_at'        => $now,
                 'updated_at'        => $now,
             ],
             [
-                'name'              => 'صحارى',
-                'url'               => 'https://laverne.com/ar/jZampKg',
-                'image_url'         => 'https://cdn.salla.sa/XzOPD/AbxIPQ2GWocwd3LANCIQlx4WQCK02yxjjzI8VTJC.png',
-                'category_url'      => 'https://laverne.com/category/yajOz',
+                'name'              => 'عطر توباكو باشا', // 5
+                'url'               => 'https://3saf.com/ar/RUFRD/p1353979568',
+                'image_url'         => 'https://cdn.salla.sa/yrlRO/DByJYgFtUHr4Vcmxrsrh4FLS3AV0vp09Uh9mVKQ4.jpg',
                 'created_at'        => $now,
                 'updated_at'        => $now,
             ],
             [
-                'name'              => 'ميموري',
-                'url'               => 'https://laverne.com/ar/ZYRaoPp',
-                'image_url'         => 'https://cdn.salla.sa/XzOPD/T8KLDWwVABwCil8f5fD1qpcPeVguL0uSzwcENoEV.png',
-                'category_url'      => 'https://laverne.com/category/yajOz',
+                'name'              => 'عطر روفان بلو سبورت', // 5
+                'url'               => 'https://3saf.com/ar/RU/p714740184',
+                'image_url'         => 'https://cdn.salla.sa/yrlRO/RsNMOVuxNemQbIf8VEkB2XL6M80boCWeEzlq8649.jpg',
                 'created_at'        => $now,
                 'updated_at'        => $now,
             ],
             [
-                'name'              => 'بوكيه',
-                'url'               => 'https://laverne.com/ar/YgYpDrW',
-                'image_url'         => 'https://cdn.salla.sa/XzOPD/nR3OiRKk8CMzlxSsC260On8YiD9J621l4tI4t6xp.jpg',
-                'category_url'      => 'https://laverne.com/category/yajOz',
+                'name'              => 'عطر ليدي', // 4
+                'url'               => 'https://3saf.com/ar/lad/p987595079',
+                'image_url'         => 'https://cdn.salla.sa/yrlRO/K8y388R3ovjxGLqY4S6Xsd2O6YeSJbF5mfgqjFPj.jpg',
                 'created_at'        => $now,
                 'updated_at'        => $now,
             ],
             [
-                'name'              => 'لومينوس',
-                'url'               => 'https://laverne.com/ar/pnxdmA',
-                'image_url'         => 'https://cdn.salla.sa/XzOPD/TqtuO7J3YtaLmZa7AEbdTf0fumZJxfCbbPttnJEp.png',
-                'category_url'      => 'https://laverne.com/category/aewqrD',
+                'name'              => 'عطر جريس اريك - 200مل', // 4
+                'url'               => 'https://3saf.com/ar/GR/p1491376332',
+                'image_url'         => 'https://cdn.salla.sa/yrlRO/7WaH5HrtcWuqyMK6bPXM7Y6Q9folYZUUu7QQUWl9.jpg',
                 'created_at'        => $now,
                 'updated_at'        => $now,
             ],
             [
-                'name'              => 'اللور',
-                'url'               => 'https://laverne.com/ar/lGboNdB',
-                'image_url'         => 'https://cdn.salla.sa/XzOPD/EzwvhJsRqHUHnIWgjGzb8JiPrLOdQrAWlr2rU3rA.png',
-                'category_url'      => 'https://laverne.com/category/aewqrD',
+                'name'              => 'عطر الكبرياء والغرور 200 مل', // 7
+                'url'               => 'https://3saf.com/ar/igo/p2025988140',
+                'image_url'         => 'https://cdn.salla.sa/yrlRO/TQTvL4ozzESLhwHkL7sNGY5Z7YvxjQ8VdSuskaD5.jpg',
                 'created_at'        => $now,
                 'updated_at'        => $now,
             ],
             [
-                'name'              => 'فوربدن',
-                'url'               => 'https://laverne.com/ar/onQYNGp',
-                'image_url'         => 'https://cdn.salla.sa/XzOPD/ZJOhtf7cwc48m9eUeaVfsPjMc6BHbwmnqgho661Y.png',
-                'category_url'      => 'https://laverne.com/category/aewqrD',
+                'name'              => 'عطر وايلد سموكي 200 مل', // 5
+                'url'               => 'https://3saf.com/ar/smok/p95857200',
+                'image_url'         => 'https://cdn.salla.sa/yrlRO/ACHntxWPn7tKOFK2Tqvalm7LpdLLy3d3jwZ33Ojj.jpg',
                 'created_at'        => $now,
                 'updated_at'        => $now,
             ],
             [
-                'name'              => 'مس لافيرن',
-                'url'               => 'https://laverne.com/ar/gZQqbXv',
-                'image_url'         => 'https://cdn.salla.sa/XzOPD/JUkZ8xp7QwgAiyMy49tWpYMiMdQEaLhUGUIHxn1y.png',
-                'category_url'      => 'https://laverne.com/category/aewqrD',
+                'name'              => 'عطر كنق', // 2
+                'url'               => 'https://3saf.com/ar/KI/p513693717',
+                'image_url'         => 'https://cdn.salla.sa/yrlRO/lL8Y8ddWNJ8UQV95Y9w1u5bBMLmyb3VhB5j4pAIv.png',
                 'created_at'        => $now,
                 'updated_at'        => $now,
             ],
             [
-                'name'              => 'ليدي روز',
-                'url'               => 'https://laverne.com/ar/YvGRxn',
-                'image_url'         => 'https://cdn.salla.sa/XzOPD/bGlU1HWwuYhv80C7D2H8g7Ll9VevfZwdEZl3LJxf.jpg',
-                'category_url'      => 'https://laverne.com/category/aewqrD',
+                'name'              => 'عطر سماء وبحر', // 1
+                'url'               => 'https://3saf.com/ar/SA/p1467822237',
+                'image_url'         => 'https://cdn.salla.sa/yrlRO/VXNBd7KDoD1NooTOmAYTX8oDzuoFggujS8Kg9plF.jpg',
                 'created_at'        => $now,
                 'updated_at'        => $now,
             ],
             [
-                'name'              => 'مسك باتشولي',
-                'url'               => 'https://laverne.com/ar/eDQqGR',
-                'image_url'         => 'https://cdn.salla.sa/XzOPD/mw0UVZkyYrxq6X1RympaOx3VsGgSZz12lbUas6lf.png',
-                'category_url'      => 'https://laverne.com/category/aewqrD',
+                'name'              => 'عطر فرانكل', // 6
+                'url'               => 'https://3saf.com/ar/Frankl/p1479518088',
+                'image_url'         => 'https://cdn.salla.sa/yrlRO/czEoM2IAjt85dktQkU3vp0W5YnlXvHgymB8d0o0n.png',
                 'created_at'        => $now,
                 'updated_at'        => $now,
             ],
             [
-                'name'              => 'بلانكا',
-                'url'               => 'https://laverne.com/ar/AzPnRgp',
-                'image_url'         => 'https://cdn.salla.sa/XzOPD/n35uNzVxyOobcZzf1wZVqB4lPMnmOhyWpfvbFiCp.png',
-                'category_url'      => 'https://laverne.com/category/aewqrD',
+                'name'              => 'عطر نوبل 200 مل', // 8
+                'url'               => 'https://3saf.com/ar/NOB/p1362089002',
+                'image_url'         => 'https://cdn.salla.sa/yrlRO/p2SRIB0xocAVR8zm01z3gDAk3SqJWK9tHX0PXbdD.jpg',
                 'created_at'        => $now,
                 'updated_at'        => $now,
             ],
-            [
-                'name'              => 'بلو لافيرن بخور- 200 مل',
-                'url'               => 'https://laverne.com/ar/KjjRvDy',
-                'image_url'         => 'https://cdn.salla.sa/XzOPD/zB5h8hq1UviMXhMLHY9KAxYOGFJjmArkoFCCvnSR.png',
-                'category_url'      => 'https://laverne.com/ar/category/xvVZqY',
-                'created_at'        => $now,
-                'updated_at'        => $now,
-            ],
-            [
-                'name'              => 'بلو لافيرن',
-                'url'               => 'https://laverne.com/ar/VwYOxW',
-                'image_url'         => 'https://cdn.salla.sa/XzOPD/TPKRh7vnade10W7pcjTXBat5dSPzvrewOM7cYJ4R.png',
-                'category_url'      => 'https://laverne.com/ar/category/xvVZqY',
-                'created_at'        => $now,
-                'updated_at'        => $now,
-            ],
-            [
-                'name'              => 'بلو لافيرن سبورت - 200 مل',
-                'url'               => 'https://laverne.com/ar/bRjdjqx',
-                'image_url'         => 'https://cdn.salla.sa/XzOPD/QRRp8elvrtBEzr3hvsNxhjOnrPcVIDAtMz5pRLIb.png',
-                'category_url'      => 'https://laverne.com/ar/category/xvVZqY',
-                'created_at'        => $now,
-                'updated_at'        => $now,
-            ],
-            [
-                'name'              => 'بلو لافيرن تايقر',
-                'url'               => 'https://laverne.com/ar/zvzbXNE',
-                'image_url'         => 'https://cdn.salla.sa/XzOPD/dX4J7y8JPRdRFv2qZvuFRHM3mZxWLbDscMsJjK9j.png',
-                'category_url'      => 'https://laverne.com/ar/category/xvVZqY',
-                'created_at'        => $now,
-                'updated_at'        => $now,
-            ]
         ]);
     }
 }
--- database/seeders/QuizPointProductTableSeeder.php
+++ database/seeders/QuizPointProductTableSeeder.php
@@ -20,15 +20,15 @@ class QuizPointProductTableSeeder extends Seeder
 
         // Key is the point reference, value is the products ids
         $choices = [
-            0 => [12],
-            1 => [16,19],
-            2 => [7,17],
-            3 => [15],
-            4 => [9,11],
-            5 => [1,3,18],
-            6 => [10,13],
-            7 => [2,4,5],
-            8 => [6,8,14],
+            0 => [3],
+            1 => [13],
+            2 => [12],
+            3 => [4,5],
+            4 => [8,9],
+            5 => [6,7,11],
+            6 => [14],
+            7 => [1,2,10],
+            8 => [15],
         ];
 
         Quiz::first()->points()->each(function (QuizPoint $point) use ($choices) {
