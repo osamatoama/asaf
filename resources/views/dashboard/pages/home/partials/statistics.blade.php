a8855c522e53fdaaa03e03c2adce4dec17a1bcd2
Mohamed Ismail <mohamedismail0994@gmail.com>
2023-12-17 02:53:44 +0200

    Update statistics.blade.php

--- resources/views/dashboard/pages/home/partials/statistics.blade.php
+++ resources/views/dashboard/pages/home/partials/statistics.blade.php
@@ -2,10 +2,10 @@
     <div class="col-lg-6 col-md-12">
         <div class="card">
             <div class="card-inner">
-                <h5 class="card-title">
+                <h5 class="card-title" style="color: #9d72ff;">
                     @lang('dashboard/statistics.' . $key)
                 </h5>
-                <p class="card-text" style="font-size: large; color: #9d72ff; font-weight: bold;">
+                <p class="card-text" style="font-size: large; font-weight: bold;">
                     {{ $statistic }}
                 </p>
             </div>
