4caa7dafb3aeba29c6f2e35d04bf3dc069b68261
kareem-torky <kareemmfouad.dev@gmail.com>
2024-01-27 05:34:04 +0200

    fix-missing-primary-key-configs-for-migration

--- database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
+++ database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -11,6 +12,11 @@ return new class extends Migration
      */
     public function up(): void
     {
+        try {
+            DB::statement('SET SESSION sql_require_primary_key=0');
+        } catch (\Throwable $th) {
+        }
+
         if (!Schema::hasTable('password_reset_tokens')) {
             Schema::create('password_reset_tokens', function (Blueprint $table) {
                 $table->string('email')->primary();
@@ -18,7 +24,6 @@ return new class extends Migration
                 $table->timestamp('created_at')->nullable();
             });
         }
-
     }
 
     /**
